bc3faf0756c36467f63617aa3bd036e1762c4e42
jdevolevol <jdevolevol@gmail.com>
2026-06-29 20:47:54 +0700

    test: Do exercise

--- ex-1.js
+++ ex-1.js
@@ -3,15 +3,21 @@ const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
 
 function addSalary5000(previousSalary) {
   // Start coding here
+  return previousSalary = previousSalary+5000;
 }
 
 function forEach(array, operation) {
   // Start coding here
+    for(let i=0;i<array.length;i++){
+      array[i] = operation(array[i]);
+    }
+
+    return array;
 }
 
 // Using `forEach` function here
 
-let newEmployeeSalaries;
+let newEmployeeSalaries = forEach(employeeSalaries, addSalary5000);
 
 console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
 
@@ -19,8 +25,10 @@ console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
 ====================================
 
 1. ใน Exercise นี้ ฟังก์ชันใดเป็น Callback Function?
+ตอบ ฟังก์ชัน addSalary5000() เป็นฟังก์ชันที่ถูกบรรจุในฟังก์ชัน forEach()
 
 2. ใน Exercise นี้ ฟังก์ชันใดเป็น Higher Order Function?
+ตอบ ฟังก์ชัน  forEach() มีการเขียน operation(....) อยู่ในbody
 
 ====================================
 */
